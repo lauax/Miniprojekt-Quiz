714c1793f386dcde11a45b3baeb1e15771de94da
ye-mou <git.ymo22@gmail.com>
2023-02-26 20:47:22 +0100

    reading the API and shuffle the ArrayOfQuestions

---
+++ src/API.ts
@@ -0,0 +1,39 @@
+import { shuffleArray } from "./Utils";
+
+
+
+export type Questions = {
+    category:string;
+    correct_answer:string;
+    difficulty:string;
+    incorrect_answers:string[];
+    question:string;
+    type:string;
+
+}
+
+export type QuestionsState = Questions & { answers:string[]}
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard",
+}
+
+
+export const fetchQuizQuestions = async (amount:number, difficulty:Difficulty) => {
+    const endpoint = 'https://opentdb.com/api.php?amount=10&type=multiple';
+
+    const data = await (await fetch(endpoint)).json();
+    return data.results.map((question:Questions) =>({
+        
+            ...question,
+            
+            answer: shuffleArray([...question.incorrect_answers, question.correct_answer]),
+        
+
+
+    }))
+};  
+
+    
