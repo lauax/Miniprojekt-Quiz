02fd94ee6f7b3bc854483057c11f8b57f9fdd363
ye-mou <git.ymo22@gmail.com>
2023-03-01 18:44:27 +0100

    added styling and installed styled components

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const router = createBrowserRouter(
-  createRouterFromElement(
-    <Route>
-        <Route index element={} />
-        <Route path="quiz" element={} />
-        <Route path="result" element={} />
-    </Route>
-  )
-);
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+   
     <App />
   </React.StrictMode>,
 )
