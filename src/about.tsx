308fbf7030ab39d59fab9e1e23ef0a9f533164b4
lauax <lucas12alfredsson@gmail.com>
2023-03-02 17:38:00 +0100

    routing färdigställde

--- src/about.tsx
+++ src/about.tsx
@@ -1,10 +1,64 @@
+
+import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { OurStyle } from "./Home.style";
+
+
+
+
 function About() {
   return (
     <div>
-      <h1>About</h1>
-      <p>This is the about page</p>
+      <Aside>
+        <Link to="/">Startpage</Link>
+        <Link to="Quiz">Quizgame</Link>
+        <Link to="about">About...</Link>
+      </Aside>
+      <Outlet />
+      <OurStyle />
     </div>
+
   );
 }
 
 export default About;
+
+const Aside = styled.aside`
+  background-color: rgb(165, 37, 150);
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  background-color: #f1f1f1;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1000;
+  overflow-y: auto;
+
+  && a {
+    display: block;
+    padding: 12px;
+    text-decoration: none;
+    color: #333;
+    transition: background-color 0.2s ease;
+    background-color: rgb(165, 37, 150);
+    background-color: rgb(165, 37, 150);
+    color: #fff;
+    border: 2px solid #f9f9f9;
+    border-radius: 40px;
+    font-size: 1.5em;
+    padding: 15px 25px;
+    margin: auto; /* This centers the button horizontally */
+  }
+
+  && a:hover {
+    background-color: #f9f9f9;
+    color: #f5c035;
+    text-shadow: 0 0 20px #f5c035, 0 0 30px #f5c035, 0 0 40px #f5c035,
+      0 0 70px #f5c035;
+    box-shadow: 0 0 20px #f5c035, 0 0 30px #f5c035, 0 0 40px #f5c035,
+      0 0 70px #f5c035;
+  }
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider,
+  RouterProvider
 } from "react-router-dom";
+import About from "./about";
 import App from "./App";
 import Home from "./Home";
 import Quiz from "./Quiz";
@@ -15,6 +16,7 @@ const routerJSX = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
       <Route path="quiz" element={<Quiz />} />
+      <Route path="about" element={<About/>} />
     </Route>
   )
 );
