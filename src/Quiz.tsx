e1a2a1b8e8f3e4d7ee440000923d2856b065967a
ye-mou <git.ymo22@gmail.com>
2023-03-02 18:13:58 +0100

    added another pag to route

--- src/Quiz.tsx
+++ src/Quiz.tsx
@@ -1,9 +1,27 @@
+import styled from "styled-components";
+
+const QuizContainer = styled.div`
+  background-color: purple;
+  color: white;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 300px;
+  width: 600px;
+  margin: auto;
+`;
+
 function Quiz() {
   return (
-    <div>
-      <h1>Quiz</h1>
-      <p>This is the quiz page</p>
-    </div>
+    <QuizContainer>
+      <h1>Quiz Game</h1>
+      <p>
+        Test your knowledge with our quiz game! Answer a series of questions on
+        a variety of topics and see how many you can get right.
+      </p>
+    </QuizContainer>
   );
 }
 
