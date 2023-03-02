f1d9e42574224e866a83ca259cf032bb00d26170
ye-mou <git.ymo22@gmail.com>
2023-03-02 01:38:54 +0100

    added logic using the itenary operator for checking the correct answer. If true:value.

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AnswerObject } from '../App';
+import { ButtonWrapper, Wrapper } from './QuestionCards.styles';
 type Props= {
     question:string;
     answers: string[];
@@ -12,7 +13,7 @@ type Props= {
 //FC är inbyggt för FunctionalComponent 
 const QuestionCard:React.FC<Props> = ({question, answers, callback, userAnswer,questionNr,totalQuestions}) => ( 
 
-<div>
+<Wrapper>
     <p className='number'>
         Question:{questionNr}/ {totalQuestions} 
     </p>  
@@ -20,15 +21,21 @@ const QuestionCard:React.FC<Props> = ({question, answers, callback, userAnswer,q
 
     <div>
         {answers.map(answer => (
-            <div key={answer}>
+            <ButtonWrapper 
+            key={answer}
+            correct = {userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+            
+            
+            >
                 <button disabled = {userAnswer ? true :false} value={answer}onClick = {callback}>
                     <span dangerouslySetInnerHTML={{__html:answer}}></span>
                 </button>
-            </div>
+            </ButtonWrapper>
                
         ))}
     </div>
-    </div>
+    </Wrapper>
 
 );
 
