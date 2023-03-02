49fb8d24d34282abf4a64afe78ebc79f51702533
lauax <lucas12alfredsson@gmail.com>
2023-03-02 11:28:30 +0100

    routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
+import App from './App';
+import './index.css';
+const router = createBrowserRouter( 
+   createRouterFromElement( 
 
-import App from './App';import './index.css';
+<Route path="/" element={App}> 
+   <Route index element={<div className='App'></div>} /> 
+   <Route path="quiz" element={} /> 
+   <Route path="result" element={} /> 
+</Route>
+)
+);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(  
-  <React.StrictMode>
-            
-     <App />
-       
-  </React.StrictMode>
-  ,)
+ReactDOM.createRoot(document.getElementById('root') as 
+HTMLElement).render( 
+   <React.StrictMode>
+      <RouterProvider router={router}/> 
+      <App/> 
+      </React.StrictMode>,
+      )
