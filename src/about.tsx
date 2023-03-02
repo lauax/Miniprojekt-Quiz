b88b6aef631b944468be8a2fc96f4342c07ed832
lauax <lucas12alfredsson@gmail.com>
2023-03-02 20:25:33 +0100

    Färdigställt spel - endscene

--- src/about.tsx
+++ src/about.tsx
@@ -1,15 +1,14 @@
 import { OurStyle } from "./Home.style";
 
+
+
+
+
 function About() {
   return (
-    <>
     <OurStyle/>
-    <div>
-      <h1>About</h1>
-      <p>This is the about page</p>
-    </div>
-    </>
   );
 }
+
 
 export default About;
