f50d4bdbd67040ab7b1ac9a76b6cc460a1da73aa
ye-mou <git.ymo22@gmail.com>
2023-05-29 01:38:10 +0200

    stylinf for QC

--- src/components/QuestionCards.styles.ts
+++ src/components/QuestionCards.styles.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  max-width: 1100px;
+  max-width: 800px;
+  margin: 0 auto;
   background: #f0e6ff;
   border-radius: 10px;
   border: 2px solid #663399;
   padding: 20px;
   box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
   text-align: center;
-  
+
   p {
     font-size: 1rem;
   }
@@ -34,15 +35,17 @@ export const ButtonWrapper = styled.div<ButtonWrapperProps>`
     height: 40px;
     margin: 5px 0;
     background: ${({ correct, userClicked }) =>
-      correct
-        ? "#8bc34a"
-        : !correct && userClicked
-        ? "#ff82b4"
-        : "#8076a3"};
+      correct ? "#8bc34a" : !correct && userClicked ? "#ff82b4" : "#8076a3"};
     border: 3px solid #fff;
     box-shadow: 1px 2px 0px rgba(0, 0, 0, 0.1);
     border-radius: 10px;
     color: #fff;
     text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.25);
+  }
+
+  @media screen and (max-width: 600px) {
+    button {
+      font-size: 0.7rem;
+    }
   }
 `;
