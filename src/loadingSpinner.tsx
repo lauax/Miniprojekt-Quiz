5588ca695fa7290953dbf2be0b798ccb79b920ea
lauax <lucas12alfredsson@gmail.com>
2023-05-30 16:22:41 +0200

    1 ny funktion inlagd

---
+++ src/loadingSpinner.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoadingSpinner: React.FC = () => {
+  return (
+    <SpinnerContainer>
+      <Spinner />
+    </SpinnerContainer>
+  );
+};
+
+const SpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Spinner = styled.div`
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export default LoadingSpinner;
